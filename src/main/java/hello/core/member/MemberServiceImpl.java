b149456fddf31db7b49d752d4bb235d3fa6d0aaf
DESKTOP-2V14N72\user <keji@eco.co.kr>
2025-06-30 15:09:56 +0900

    회원 도메인 개발

---
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,18 @@
+package hello.core.member;
+
+// 회원 서비스 - 회원 서비스 구현체
+public class MemberServiceImpl implements MemberService{
+
+    // 구현 객체 선택
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
