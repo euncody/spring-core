e24a8a96b8b2e3615ba4e5308c7d86898256560e
DESKTOP-2V14N72\user <keji@eco.co.kr>
2025-06-30 16:35:18 +0900

    주문과 할인 도메인 개발

---
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -0,0 +1,18 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy{
+
+    private int discountFixAmount = 1000; // 고정 할인 금액 (1000원 할인)
+
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade() == Grade.VIP){ // ENUM은 비교 연산자(==)로 비교 가능
+            return discountFixAmount; // VIP 회원에게는 고정 할인 금액을 반환
+        } else {
+            return 0; // 일반 회원에게는 할인 없음
+        }
+    }
+}
