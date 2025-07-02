680419486e1d6083f7717425df9c174d33be6fbd
DESKTOP-2V14N72\user <keji@eco.co.kr>
2025-07-02 11:05:02 +0900

    주문과 할인 도메인 실행과 테스트

---
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,27 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getMemberId()).isEqualTo(memberId);
+        // Assertions는 테스트 코드에서 기대하는 값과 실제 값이 일치하는지 확인하는 역할
+        // 같으면 테스트 성공 (pass).
+        // 다르면 테스트가 실패 (fail) 하며, JUnit이 실패로 보고
+    }
+}
